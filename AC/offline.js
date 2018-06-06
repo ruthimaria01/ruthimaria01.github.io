56e0a202141d4285461a82431adfa6cf84cd4d1a
alicinhaa <aliceoliveira.maso@gmail.com>
2018-06-06 19:56:49 -0300

    jogo

---
+++ AC/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1528325698,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/logomaker-sheet0.png",
+		"images/funcocred-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"media/errou - faustao falando_128k.m4a",
+		"media/errou - faustao falando_128k.ogg",
+		"media/aplausos . ( memes do youtube ) @2 + download na descrição ..m4a",
+		"media/aplausos . ( memes do youtube ) @2 + download na descrição ..ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
